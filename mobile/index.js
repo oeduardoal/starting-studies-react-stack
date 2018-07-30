03ff82639aab2182c4e541496e95ea559587118d
Eduardo Almeida <oeduardoal@gmail.com>
2018-07-30 12:17:09 -0300

    starting-studies-react-stack

---
+++ mobile/index.js
@@ -0,0 +1,9 @@
+import 'config/Reactron';
+
+import { AppRegistry } from 'react-native';
+
+import App from 'components/App';
+
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ mobile/src/components/App.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const backgroundColor = '#EE7777';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor,
+  },
+  containerPosts: {
+    flex: 1,
+    backgroundColor,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
